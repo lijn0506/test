7d5d4470b36ef5a0d7d9492fbeae9305decc9e05
Administrator <Administrator@ZLWin7>
2017-01-04 18:02:01 +0800

    this is test sysout

---
+++ src/main/java/com/lijn/test/Test.java
@@ -0,0 +1,12 @@
+package com.lijn.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test");
+		  
+		System.out.println("this is test");
+	}
+
+}
